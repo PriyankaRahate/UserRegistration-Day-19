ab7752b28575f639520575233d02d3500a9411ad
unknown <priyanka.rahate23@gmail.com>
2022-04-12 14:45:44 +0530

    ValidPassword

--- src/main/java/userRegistrationday19/UserRegistration.java
+++ src/main/java/userRegistrationday19/UserRegistration.java
@@ -30,13 +30,18 @@ public class UserRegistration {
             System.out.println(Pattern.matches("^(91)[\\s]{1}[1-9]{1}[0-9]{9}$", phone));
     }
 
+        public void validPassword(){
+            System.out.print("Enter password : ");
+            String password = scanner.nextLine();
+            System.out.println(Pattern.matches("^[A-Za-z0-9].{8,}$", password));
+    }
+
     public static void main(String[] args) {
             UserRegistration userRegistration = new UserRegistration();
             userRegistration.validFirstName();
             userRegistration.validLastName();
             userRegistration.validEmail();
             userRegistration.validPhoneNumber();
+            userRegistration.validPassword();
 }
-
-
 }
